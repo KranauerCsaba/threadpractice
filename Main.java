0ed8a92b8f7c59004dc12c36bc8e9aa404327b3d
krana <krana.csaba@gmail.com>
2021-06-26 12:17:32 +0200

    syncronized practice

--- Main.java
+++ Main.java
@@ -1,36 +1,25 @@
-import java.util.Scanner;
 
-public class Main implements Runnable {
-    public static int amount = 0;
 
-    private volatile boolean runner = true;
+public class Main  {
+    
+    private int count;
   
     public static void main(String[] args) {
-      Main m = new Main();
-      Thread t = new Thread(m);
-      t.start();
-      System.out.println("Press enter to pause!");
-      Scanner sc = new Scanner(System.in);
-      sc.nextLine();
-      m.kill();
-
-
-    }
-    public void run() {
-        while(runner){
-            System.out.println("Futok");
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
       
-    }
+            Thread t1 = new Thread( new Runnable(){
+                public void run(){
 
-    public void kill(){
-        this.runner = false;
-        System.out.println("Stop!");
+                }
+            });
+
+            Thread t1 = new Thread( new Runnable(){
+                public void run(){
+                    
+                }
+            });
+
     }
+    
+
+    
   }
